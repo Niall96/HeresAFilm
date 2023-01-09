1742ed4116620175c50173aa9ba8b380bc5ec650
Niall96 <43571706+Niall96@users.noreply.github.com>
2023-01-09 16:03:31 +0000

    Update V1.0__initial_migration.sql

--- migration/main/V1.0__initial_migration.sql
+++ migration/main/V1.0__initial_migration.sql
@@ -1,1 +1,39 @@
+CREATE TABLE user (
+    id serial PRIMARY KEY,
+    email_address varchar NOT NULL,
+    user_name varchar NOT NULL,
+    password varchar NOT NULL,
+    date_of_birth date,
+    created timestamp DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE user_film_suggestion_list (
+  user_id integer, 
+  film_id array, 
+  CONSTRAINT fk_user FOREIGN KEY (user_id)
+      REFERENCES user(id)
+  CONSTRAINT fk_film FOREIGN KEY (film_id)
+      REFERENCES film(id)
+  );
+  
+  CREATE TABLE user_film_watched_list (
+  user_id integer, 
+  film_id array, 
+  CONSTRAINT fk_user FOREIGN KEY (user_id)
+      REFERENCES user(id)
+  CONSTRAINT fk_film FOREIGN KEY (film_id)
+      REFERENCES film(id)
+  );
+  
+  CREATE TABLE user_film_favourite_list (
+  user_id integer, 
+  film_id array, 
+  CONSTRAINT fk_user FOREIGN KEY (user_id)
+      REFERENCES user(id)
+  CONSTRAINT fk_film FOREIGN KEY (film_id)
+      REFERENCES film(id)
+  );
+
+  CREATE TABLE film (
+    
+  );
