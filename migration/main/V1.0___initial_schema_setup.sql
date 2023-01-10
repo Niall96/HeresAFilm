91406afbf3e0add428d38e3b04ed2fd3535af2bd
Niall96 <43571706+Niall96@users.noreply.github.com>
2023-01-10 10:54:28 +0000

    Update V1.0___initial_schema_setup.sql

--- migration/main/V1.0___initial_schema_setup.sql
+++ migration/main/V1.0___initial_schema_setup.sql
@@ -1,8 +1,58 @@
-CREATE TABLE user (
-    id serial PRIMARY KEY,
-    email_address varchar NOT NULL,
-    user_name varchar NOT NULL,
-    password varchar NOT NULL,
-    date_of_birth date,
-    created timestamp DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS Users(
+id SERIAL PRIMARY KEY,
+email_address varchar NOT NULL,
+user_name varchar NOT NULL,
+password varchar NOT NULL,
+date_of_birth date,
+created timestamp DEFAULT CURRENT_TIMESTAMP 
+);
+
+CREATE TABLE IF NOT EXISTS Film(
+id SERIAL PRIMARY KEY,
+film_name VARCHAR NOT NULL,
+synopsis VARCHAR NOT NULL,
+genre VARCHAR NOT NULL,
+release_date DATE,
+rating DECIMAL NOT NULL,
+image VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Film_review(
+id SERIAL PRIMARY KEY,
+film_id int,
+CONSTRAINT film_id FOREIGN KEY (film_id) REFERENCES film (id),
+user_id int,
+CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id),
+description VARCHAR NOT NULL,
+rating DECIMAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS actors(
+id SERIAL PRIMARY KEY,
+actor_name VARCHAR NOT NULL,
+image VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Film_actors(
+film_id int,
+CONSTRAINT film_id FOREIGN KEY (film_id) REFERENCES film (id),
+actor_id int ARRAY
+);
+
+CREATE TABLE IF NOT EXISTS user_film_suggestion_list(
+user_id int, 
+CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id),
+film_id int ARRAY
+);
+
+CREATE TABLE IF NOT EXISTS user_film_watched_list(
+user_id int, 
+CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id),
+film_id int ARRAY
+);
+
+CREATE TABLE IF NOT EXISTS user_film_favourite_list(
+user_id int, 
+CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id),
+film_id int ARRAY
 );
