f0ec4d7235b53d850682357fbff215f7ff3b9c5e
Niall96 <43571706+Niall96@users.noreply.github.com>
2023-01-10 14:20:08 +0000

    add initial data sql script

---
+++ migration/main/V1.1__initial_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO public.film (id, film_name, synopsis, genre, release_date , rating, image) VALUES
+(1, 'The Shawshank Redemption', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.' ,'Drama', '1994-09-10',9.3,'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i'),
+(2, 'The Godfather', 'The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.', 'Crime', '1972-03-14', 9.2, 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i'),
+(3, 'The Dark Knight', 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 'Superhero', '2008-07-28', 9.0, 'https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632/?ref_=tt_ov_i'),
+(4, 'Pulp Fiction', 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.', 'Crime', '1994-05-21', 8.9, 'https://www.imdb.com/title/tt0110912/mediaviewer/rm2112395009/?ref_=tt_ov_i'),
+(5, 'Forrest Gump','The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.', 'Drama', '1994-06-23', 8.8, 'https://www.imdb.com/title/tt0109830/mediaviewer/rm1954748672/?ref_=tt_ov_i'),
+(6, 'Back To The Future','Marty McFly, a 17-year-old high school student, is accidentally sent 30 years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.', 'Adventure', '1985-07-03', 8.5, 'https://www.imdb.com/title/tt0088763/mediaviewer/rm351541248/?ref_=tt_ov_i'),
+(7, 'Avengers: Infinity War','The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.','Superhero','2018-04-23',8.4,'https://www.imdb.com/title/tt4154756/mediaviewer/rm4044245504/?ref_=tt_ov_i'),
+(8, 'Coco','Aspiring musician Miguel, confronted with his family ancestral ban on music, enters the Land of the Dead to find his great-great-grandfather, a legendary singer.', 'Animation', '2017-11-22',8.4, 'https://www.imdb.com/title/tt2380307/mediaviewer/rm585455872/?ref_=tt_ov_i'),
+(9, 'Toy Story','A cowboy doll is profoundly threatened and jealous when a new spaceman action figure supplants him as top toy in a boys bedroom.','Animation', '1995-11-19',8.3, 'https://www.imdb.com/title/tt0114709/mediaviewer/rm3813007616/?ref_=tt_ov_i'),
+(10, 'Scarface', 'In 1980 Miami, a determined Cuban immigrant takes over a drug cartel and succumbs to greed.', 'Crime', '1983-12-01', 8.3, 'https://www.imdb.com/title/tt0086250/mediaviewer/rm512766208/?ref_=tt_ov_i');
+
+INSERT INTO public.actors (id, actor_name, image) VALUES
+(1,'Marlon Brando','https://www.imdb.com/name/nm0000008/mediaviewer/rm1238402304/?ref_=nm_ov_ph'),
+(2,'Al Pacino','https://www.imdb.com/name/nm0000199/mediaviewer/rm3894385408/?ref_=nm_ov_ph'),
+(3,'Diane Keaton','https://www.imdb.com/name/nm0000473/mediaviewer/rm994733056/?ref_=nm_ov_ph'),
+(4,'Tim Robbins','https://www.imdb.com/name/nm0000209/mediaviewer/rm3789593344/?ref_=nm_ov_ph'),
+(5,'Morgan Freeman','https://www.imdb.com/name/nm0000151/mediaviewer/rm3587479040/?ref_=nm_ov_ph'),
+(6,'Christian Bale','https://www.imdb.com/name/nm0000288/mediaviewer/rm3114052352/?ref_=nm_ov_ph'),
+(7,'Heath Ledger', 'https://www.imdb.com/name/nm0005132/mediaviewer/rm1308400384/?ref_=nm_ov_ph'),
+(8,'Michael Caine','https://www.imdb.com/name/nm0000323/mediaviewer/rm3495729664/?ref_=nm_ov_ph'),
+(9,'Maggie Gyllenhaal','https://www.imdb.com/name/nm0350454/mediaviewer/rm2131660544/?ref_=nm_ov_ph');
+
+
